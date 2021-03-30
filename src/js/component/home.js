fb81930c78052aebf3ddcbb191f9306cb0cbbd4f
davidgq10 <davidgq10@hotmail.com>
2021-03-30 00:05:07 +0000

    se añade fetch

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,7 +7,13 @@ export function Home() {
 	const [task, setTask] = useState("");
 	const [hoverli, setHoverli] = useState(false);
 
+	//Al iniciar consulta las tareas del usuario
 	useEffect(() => {
+		getTaskList();
+	}, []); //-> Se ejecuta solo al iniciar
+
+	//Función para obtener lista de tareas de la DB y asignarla al hook >tasklist<
+	const getTaskList = () => {
 		var myHeaders = new Headers();
 		myHeaders.append("Content-Type", "application/json");
 
@@ -21,10 +27,33 @@ export function Home() {
 			"https://assets.breatheco.de/apis/fake/todos/user/davidgq",
 			requestOptions
 		)
-			.then(response => response.text())
-			.then(result => console.log(result))
+			.then(response => response.json())
+			.then(result => setTaskList(result))
 			.catch(error => console.log("error", error));
-	}, []); // Se ejecuta solo al iniciar
+	};
+
+	// función para actualizar las tareas de la DB
+	function putTaskList() {
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify(tasklist);
+
+		var requestOptions = {
+			method: "PUT",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/davidgq",
+			requestOptions
+		)
+			.then(response => response.json())
+			.then(result => setTaskList(result))
+			.catch(error => console.log("error", error));
+	}
 
 	//Evento que se genera al presionar una tecla del input newtask
 	const handleOnKeyPress = e => {
@@ -33,11 +62,12 @@ export function Home() {
 			e.preventDefault();
 			//define la newTask que será añadida al array
 			const newTask = {
-				id: new Date().getTime(),
-				description: task
+				label: task,
+				done: false
 			};
 			//clona el tasklist actual y le añade la newTask
 			setTaskList([...tasklist].concat(newTask));
+			putTaskList();
 			//reinicia el valor de task
 			setTask("");
 		} else if (e.key === "Enter" && task == "") {
@@ -47,24 +77,26 @@ export function Home() {
 
 	const generarLista = () => {
 		//recorre el objeto y genera los elementos de la lista
-		return tasklist.map(task => (
-			<li
-				key={task.id}
-				className="list-group-item"
-				onMouseEnter={() => setHoverli(task.id)}>
-				<p className="d-inline-block text-secondary ml-4 fs-3 align-middle rounded-0">
-					{task.description}
-				</p>
-				{task.id == hoverli ? (
-					<button
-						type="button"
-						className="btn btn-light float-right"
-						onClick={() => deleteTask(task.id)}>
-						<i className="fas fa-times"></i>
-					</button>
-				) : null}
-			</li>
-		));
+		return tasklist.map((task, index) => {
+			return (
+				<li
+					key={index}
+					className="list-group-item"
+					onMouseEnter={() => setHoverli(index)}>
+					<p className="d-inline-block text-secondary ml-4 fs-3 align-middle rounded-0">
+						{task.label}
+					</p>
+					{index == hoverli ? (
+						<button
+							type="button"
+							className="btn btn-light float-right"
+							onClick={() => deleteTask(index)}>
+							<i className="fas fa-times"></i>
+						</button>
+					) : null}
+				</li>
+			);
+		});
 	};
 
 	// función para eliminar task al dar click al button
@@ -75,28 +107,33 @@ export function Home() {
 
 	//genera el componente
 	return (
-		<div className="container">
+		<div className="container mt-4">
 			<h1 className="text-muted text-center display-4">Todos</h1>
-			<input
-				type="text"
-				placeholder="Type a new task"
-				className="form-control text-secondary rounded-0"
-				value={task}
-				onChange={e => setTask(e.target.value)}
-				onKeyPress={e => handleOnKeyPress(e)}
-			/>
-			<ul className="list-group">
-				{generarLista()}
-				<div>
-					<label htmlFor="list-group-item">
-						<p className="text-secondary ml-5 mt-2">
+			<div className="d-flex flex-row">
+				<input
+					type="text"
+					placeholder="Type a new task"
+					className="form-control text-secondary rounded-0"
+					value={task}
+					onChange={e => setTask(e.target.value)}
+					onKeyPress={e => handleOnKeyPress(e)}
+				/>
+				<button className="btn btn-primary col-md-auto">
+					Delete all
+				</button>
+			</div>
+			<ul className="list-group">{generarLista()}</ul>
+			<div>
+				<label htmlFor="list-group-item">
+					<p className="text-secondary ml-5 mt-4">
+						<i>
 							{tasklist.length == 0
 								? "No tasks, add a task"
 								: tasklist.length + " item left"}
-						</p>
-					</label>
-				</div>
-			</ul>
+						</i>
+					</p>
+				</label>
+			</div>
 		</div>
 	);
 }
